8b609ac2b1ca47b121e2a7909e9746b7c2fcb7fc
Puker Who <nawwesele@gmail.com>
2024-05-28 14:05:26 +0300

    edit

--- page.php
+++ page.php
@@ -1,28 +1,22 @@
-<?php get_header(); ?>
+<?php get_header();  ?>
 
-<div class="container py-10">
-  <div class="flex flex-wrap xl:-mx-3">
-    <div class="w-full xl:w-2/12 xl:px-3">
-      <!-- all categories -->
-        <?php echo get_template_part('template-parts/components/allcategories-block'); ?>
-      <!-- end all categories -->
-    </div>
-    <div class="w-full xl:w-8/12 xl:px-3">
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <h1 class="text-2xl xl:text-3xl font-medium mb-6"><?php the_title(); ?></h1>
-        <div class="content">
-          <?php the_content(); ?>
-        </div>
-      <?php endwhile; endif; wp_reset_postdata(); ?>
-    </div>
-    <div class="w-full xl:w-2/12 xl:px-3">
-      <div>
-        <?php echo get_template_part('template-parts/components/allcity-block'); ?>
-        <?php echo get_template_part('template-parts/components/allhotels-block'); ?>
+<div class="top-block mb-16">
+  <div class="container mx-auto">
+    <h1 class="text-4xl lg:text-6xl"><?php the_title(); ?></h1>
+  </div>
+</div>
+
+<div class="container mb-16">
+  <div class="flex flex-wrap lg:-mx-6 mb-12 lg:mb-0">
+    <div class="w-full lg:w-8/12 lg:px-6 mb-6 lg:mb-0">
+      <div class="bg-white rounded-lg p-4 mb-10">
+        <?php the_content(); ?>
       </div>
     </div>
+    <div class="w-full lg:w-4/12 lg:px-6">
+      <?php get_template_part('template-parts/sidebar'); ?>
+    </div>
   </div>
-  
 </div>
 
 <?php get_footer(); ?>
