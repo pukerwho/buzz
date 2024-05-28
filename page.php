4f3aa948ed754ee0d482abf6754bdab99dc10e58
Puker Who <nawwesele@gmail.com>
2024-05-28 14:08:47 +0300

    edit

--- page.php
+++ page.php
@@ -9,7 +9,7 @@
 <div class="container mb-16">
   <div class="flex flex-wrap lg:-mx-6 mb-12 lg:mb-0">
     <div class="w-full lg:w-8/12 lg:px-6 mb-6 lg:mb-0">
-      <div class="bg-white rounded-lg p-4 mb-10">
+      <div class="content bg-white rounded-lg p-4 mb-10">
         <?php the_content(); ?>
       </div>
     </div>
